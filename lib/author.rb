8bcbb8ecf4b07c8fc16886cefc0c8d5c80247d77
caicindy87 <caicindy87@gmail.com>
2020-03-14 14:21:51 -0700

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        Post.all
+    end
+
+    def add_post(title)
+        title.author = self
+    end
+
+    def add_post_by_title(post_title)
+        post = Post.new(post_title)
+        post.author = self
+    end
+
+    def self.post_count
+        Post.all.count
+    end
+end
